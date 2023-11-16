01aa8c9c2e8da2d0c3fff6b92f7deb7c2e88542b
Soham Chowdhury <evertedsphere@gmail.com>
2023-11-16 22:33:55 +0100

    feat(furi): prototype backtracking-based annotation algorithm
    This is a simple DFS with some tweaks.

--- src/furi.rs
+++ src/furi.rs
@@ -1,11 +1,13 @@
 use anyhow::bail;
 use anyhow::Context;
 use anyhow::Result;
+use lazy_static::lazy_static;
 use regex::Regex;
 use serde::Deserialize;
 use std::collections::HashMap;
 use tracing::debug;
 use tracing::error;
+use tracing::warn;
 
 #[derive(Deserialize)]
 pub struct KanjiDic(HashMap<char, Vec<String>>);
@@ -40,109 +42,127 @@ fn longest_prefix(x: &str, ys: &[String]) -> (String, String) {
 }
 
 /// Yes, this doesn't take weird shit into account, I know
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Span {
     Kana { kana: char },
-    Furi { kanji: char, reading: String },
+    Furi { kanji: char, yomi: String },
+}
+
+// #[derive(Debug)]
+// pub struct Annotation(Vec<Span>);
+
+lazy_static! {
+    static ref KANJI_REGEX: Regex = Regex::new(r"\p{Unified_Ideograph}").unwrap();
+}
+
+#[inline]
+fn is_kanji(c: char) -> bool {
+    // kanji are always 3 bytes long (<=?)
+    let mut buf = [0; 3];
+    let s = c.encode_utf8(&mut buf);
+    KANJI_REGEX.is_match(s)
+}
+
+#[test]
+fn annotate_simple() {
+    let kd = read_kanjidic().unwrap();
+    let words = vec![
+        ("検討", "けんとう"),
+        ("人か人", "ひとかひと"),
+        // ("人人", "ひとびと"),
+        ("口血", "くち"),
+        // ("人", "ひとこと"),
+    ];
+
+    for (spelling, reading) in words {
+        let furi = furu(&spelling, &reading, &kd).context("failed to apply furi");
+        assert!(furi.is_ok());
+    }
 }
 
 pub fn furu(spelling: &str, reading: &str, kd: &KanjiDic) -> Result<Vec<Span>> {
-    let mut r = Vec::new();
+    println!("");
+    println!("");
+    let mut ret = Vec::new();
 
-    let mut rest = reading.to_owned();
+    // FIXME take '.' and '-' into account
 
-    for c in spelling.chars() {
-        let re = Regex::new(r"\p{Unified_Ideograph}").unwrap();
-        let is_kanji = re.is_match(&format!("{}", c));
-        if is_kanji {
-            match kd.0.get(&c) {
-                Some(readings) => {
-                    let (prefix, new_rest) = longest_prefix(&rest, readings.as_ref());
-                    if prefix.is_empty() {
-                        bail!("ran out of characters in reading")
-                    }
-                    debug!("picking prefix {}", prefix);
-                    r.push(Span::Furi {
-                        kanji: c,
-                        reading: prefix,
-                    });
-                    rest = new_rest;
-                    // if rest.is_empty() {
-                    //     break;
-                    // }
+    let orth = spelling.chars().collect::<Vec<_>>();
+    let pron = reading.chars().collect::<Vec<_>>();
+
+    let orth_len = orth.len();
+    let pron_len = pron.len();
+
+    // "nodes" are pairs of (0 < j < n_orth, 0 < j < n_pron)
+
+    let mut frontier = Vec::new();
+    frontier.push((0, 0, Vec::<Span>::new()));
+    debug!("orth: {:?} (len {})", orth, orth_len);
+    debug!("pron: {:?} (len {})", pron, pron_len);
+
+    // use a single path
+    // let mut history = Vec::new();
+
+    while let Some((orth_ix, pron_ix, path)) = frontier.pop() {
+        debug!("visiting {}, {} at {:?}", orth_ix, pron_ix, path);
+
+        let orth_end = orth_ix == orth_len;
+        let pron_end = pron_ix == pron_len;
+
+        if orth_end && pron_end {
+            debug!("done: {:?}", path);
+            ret = path;
+            break;
+        }
+
+        if orth_end ^ pron_end {
+            warn!("backtracking: orth_end {}, pron_end {}", orth_end, pron_end);
+            continue;
+        }
+
+        let orth_char = orth[orth_ix];
+
+        if !is_kanji(orth_char) {
+            // the checking is far simpler here: try to match one character
+            if orth[orth_ix] == pron[pron_ix] {
+                let mut new_path = path.clone();
+                new_path.push(Span::Furi {
+                    kanji: orth_char,
+                    yomi: reading.to_string(),
+                });
+                frontier.push((orth_ix + 1, pron_ix + 1, new_path))
+            }
+        } else {
+            let readings = kd.0.get(&orth_char).unwrap();
+            debug!(
+                "{} has {} readings: {:?}",
+                orth_char,
+                readings.len(),
+                readings
+            );
+            for reading in readings {
+                let rd_len = reading.chars().count();
+                if rd_len > pron_len - pron_ix {
                     continue;
                 }
-                None => {
-                    bail!("unknown kanji {}", c);
+                let candidate_slice = &pron[pron_ix..pron_ix + rd_len];
+                let matches = reading.chars().zip(candidate_slice).all(|(x, &y)| x == y);
+                if matches {
+                    // debug!("candidate next reading: {}", reading);
+                    let mut new_path = path.clone();
+                    new_path.push(Span::Furi {
+                        kanji: orth_char,
+                        yomi: reading.to_string(),
+                    });
+                    frontier.push((orth_ix + 1, pron_ix + rd_len, new_path))
                 }
             }
         }
-        match rest.chars().next() {
-            None => error!("empty"),
-            Some(c) => {
-                let (prefix, new_rest) = rest.split_at(c.len_utf8());
-                debug_assert!(prefix.len() == 1);
-                // assert c is a single character
-                let rc = prefix.chars().next();
-                if rc != Some(c) {
-                    bail!("non-matching char for kana {:?} != {}", rc, c);
-                } else {
-                    r.push(Span::Kana { kana: c });
-                    rest = new_rest.to_string();
-                }
-            }
+
+        if frontier.is_empty() {
+            bail!("failed to find matching")
         }
     }
 
-    Ok(r)
+    Ok(ret)
 }
-// pub fn furu(spelling: &str, reading: &str, kd: &KanjiDic) -> Option<Vec<Span>> {
-//     let mut r = Vec::new();
-
-//     let mut rest = reading.to_owned();
-
-//     for c in spelling.chars() {
-//         let re = Regex::new(r"\p{Unified_Ideograph}").unwrap();
-//         let is_kanji = re.is_match(&format!("{}", c));
-//         if is_kanji {
-//             match kd.0.get(&c) {
-//                 Some(readings) => {
-//                     let (prefix, new_rest) = longest_prefix(&rest, readings.as_ref());
-//                     if prefix.is_empty() {
-//                         return None;
-//                     }
-//                     r.push(Span::Furi {
-//                         kanji: c,
-//                         reading: prefix,
-//                     });
-//                     rest = new_rest;
-//                     // if rest.is_empty() {
-//                     //     break;
-//                     // }
-//                     continue;
-//                 }
-//                 None => {
-//                     error!("unknown kanji {}", c);
-//                 }
-//             }
-//         }
-//         match rest.chars().next() {
-//             None => error!("empty"),
-//             Some(c) => {
-//                 let (prefix, new_rest) = rest.split_at(c.len_utf8());
-//                 debug_assert!(prefix.len() == 1);
-//                 // assert c is a single character
-//                 let rc = prefix.chars().next();
-//                 if rc != Some(c) {
-//                     error!("non-matching chars");
-//                     return None;
-//                 } else {
-//                     r.push(Span::Kana { kana: c });
-//                     rest = new_rest.to_string();
-//                 }
-//             }
-//         }
-//     }
-
-//     Some(r)
-// }
