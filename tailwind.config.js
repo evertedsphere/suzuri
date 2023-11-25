ccfdd87d69c1b4c57bfe26ba07e24c5cdcf3fde4
Soham Chowdhury <evertedsphere@gmail.com>
2023-11-26 00:00:00 +0100

    feat(gui): nice sidebar box shadow
    "tasteful", some would call it

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/handlers.rs"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'left-side': '0 0 20px 0 rgba(0, 0, 0, 0.1), 0 0 6px 0 rgba(0, 0, 0, 0.25)',
+      }
+    },
   },
   plugins: [],
 }
