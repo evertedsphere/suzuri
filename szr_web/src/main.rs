18e9a00971ba7ef2df51bab5446f08bf58bc8ee0
Soham Chowdhury <evertedsphere@gmail.com>
2023-12-02 00:00:00 +0100

    chore: remove luw fusion

--- szr_web/src/main.rs
+++ szr_web/src/main.rs
@@ -36,53 +36,38 @@ fn main() -> Result<(), Whatever> {
     let _ = get_term(&mut conn, spelling, spelling);
 
     let text = r#"南に二十メートルほど歩いたところで、太い道路に出た。新大橋通りだ。左に、つまり東へ進めば江戸川区に向かい、西へ行けば日本橋に出る。日本橋の手前には隅田川があり、それを渡るのが新大橋だ。石神の職場へ行くには、このまま真っ直ぐ南下するのが最短だ。数百メートル行けば、清澄庭園という公園に突き当たる。その手前にある私立高校が彼の職場だった。つまり彼は教師だった。数学を教えている。石神は目の前の信号が赤になるのを見て、右に曲がった。新大橋に向かって歩いた。向かい風が彼のコートをはためかせた。彼は両手をポケットに突っ込み、身体をやや前屈みにして足を送りだした。厚い雲が空を覆っていた。その色を反射させ、隅田川も濁った色に見えた。小さな船が上流に向かって進んでいく。それを眺めながら石神は新大橋を渡った。"#;
-    let text = "世界人権宣言は、この宣言の後に国際連合で結ばれた人権規約の基礎となっており、世界の人権に関する規律の中でもっとも基本的な意義を有する。国際連合経済社会理事会の機能委員会として1946年に国際連合人権委員会が設置されると、同委員会は国際人権章典と呼ばれる単一規範の作成を目指し起草委員会を設置したが、権利の範囲や拘束力の有無を巡って意見が対立し作成のめどが立たなかったため、いったん基礎となる宣言を採択し、その後それを補強する複数の条約及び実施措置を採択することとなった。";
+    // let text = "世界人権宣言は、この宣言の後に国際連合で結ばれた人権規約の基礎となっており、世界の人権に関する規律の中でもっとも基本的な意義を有する。国際連合経済社会理事会の機能委員会として1946年に国際連合人権委員会が設置されると、同委員会は国際人権章典と呼ばれる単一規範の作成を目指し起草委員会を設置したが、権利の範囲や拘束力の有無を巡って意見が対立し作成のめどが立たなかったため、いったん基礎となる宣言を採択し、その後それを補強する複数の条約及び実施措置を採択することとなった。";
     // let text = "中途半端はしないで";
     // let text = "しないで";
 
     let mut session = UnidicSession::new()?;
     let res = session.tokenise_mut(&text)?;
+
     println!("{}\n", res);
 
     let dict = read_dictionary("input/jmdict_en").whatever_context("read dict")?;
 
-    let mut out: (String, String, String) = Default::default();
-    let mut v = Vec::new();
-    let mut fused_count = 0;
-
     for AnnToken {
-        token,
+        lemma_spelling,
+        lemma_reading,
         spelling,
         reading,
         ..
     } in res.0.into_iter()
     {
-        let reading: String = reading.chars().map(kata_to_hira).collect();
-        let candidate = (
-            (out.0.clone() + "-" + token),
-            (out.1.clone() + &spelling),
-            (out.2.clone() + &reading),
-        );
+        let lemma_reading: String = if lemma_reading == lemma_spelling {
+            lemma_reading
+        } else {
+            lemma_reading.chars().map(kata_to_hira).collect()
+        };
         if let Some(_) = dict
             .iter()
-            .find(|term| term.spelling == candidate.1 && term.reading == candidate.2)
+            .find(|term| term.spelling == lemma_spelling && term.reading == lemma_reading)
         {
-            out = candidate;
-            fused_count += 1;
+            //
         } else {
-            if fused_count > 1 {
-                // debug!("pushing fused word {:?}", out);
-            }
-            v.push(out.clone());
-            out = (token.to_owned(), spelling, reading);
-            fused_count = 1;
+            // warn!("term not found: {}({})", lemma_spelling, lemma_reading);
         }
-    }
-    v.push(out.clone());
-    // v.push((fused_spelling.clone(), fused_reading.clone()));
-    debug!("done");
-    for (t, _s, _r) in v.iter() {
-        print!("{} ", t);
     }
 
     Ok(())
